cc958b1ec93da5014b62742d507391172acab8b2
MV0710 <maksimverlevskiy@gmail.com>
2024-11-18 18:36:07 +0300

    Update Verlevsky_Lab1.cpp
    Добавлено меню ввода на русском

--- Verlevsky_Lab1/Verlevsky_Lab1/Verlevsky_Lab1.cpp
+++ Verlevsky_Lab1/Verlevsky_Lab1/Verlevsky_Lab1.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+using namespace std;
+struct Pipe {
+
+};
+int main() {
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
+	wcout << L"Добрый День! Выберете Действие из предложенных: \n1.Добавить трубу \n2.Добавить КС \n3.Просмотр всех объектов \n4.Редактировать трубу \n5.Редактировать КС \n6.Сохранить \n7.Загрузить \n0.Выход" << endl;
+	int n=-1;
+	while (n != 0) {
+		wcin >> n;
+	}
+	wcout << L"Работа окончена" << endl;
+	return 0;
+}
